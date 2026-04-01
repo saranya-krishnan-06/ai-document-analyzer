86d0e3d2a0dbf472f00e008c3cb344cc69f1c7ec
saranya <saranyakrishnanadr@gmail.com>
2026-04-01 12:12:58 -0400

    add app screenshot

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@
 import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
 from app.routes.analyze import router as analyze_router
 from app.routes.chat import router as chat_router
 
@@ -13,15 +13,16 @@ logger = logging.getLogger(__name__)
 templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI(title="AI Document Analyzer")
-
-# Allow S3 frontend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # replace * with your S3 URL once you have it
+    allow_origins=[
+        "https://dwlf85sg0bufi.cloudfront.net",
+        "https://ai-doc-analyzer-frontend.s3.us-east-2.amazonaws.com",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(analyze_router, prefix="/analyze", tags=["Analyze"])
 app.include_router(chat_router, prefix="/ai", tags=["Chat"])
 
